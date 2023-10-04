f8a2831cc48438f836569e7f3a49e58e22b378f3
Sans2k <140954881+Sans2k@users.noreply.github.com>
2023-10-04 14:54:28 +0700

    Add files via upload

---
+++ arrow_function.js
@@ -0,0 +1,10 @@
+const login = () => {
+    console.log("----------------")
+    console.log("      login     ")
+    console.log("----------------")
+    console.log("username: ")
+    console.log("password: ")
+    console.log("----------------")
+}
+
+login()
---
+++ profile_reg_function.js
@@ -0,0 +1,10 @@
+function login(){
+    console.log("----------------")
+    console.log("      login     ")
+    console.log("----------------")
+    console.log("username: ")
+    console.log("password: ")
+    console.log("----------------")
+}
+
+login()
